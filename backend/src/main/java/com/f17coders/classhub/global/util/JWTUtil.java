a150a8555f66c5fd2dc6c11f83f6aa3f20816a8d
정승환 <seunghw2@naver.com>
2024-02-04 20:56:50 +0900

    [fix] Security 관련 코드 rollback * [fix] Security 관련 코드 rollback

--- backend/src/main/java/com/f17coders/classhub/global/util/JWTUtil.java
+++ backend/src/main/java/com/f17coders/classhub/global/util/JWTUtil.java
@@ -8,9 +8,8 @@ import java.util.HashMap;
 import java.util.Map;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
 
-@Component
+//@Component
 @Log4j2
 public class JWTUtil {
     @Value(value = "${key.jwt.secret}")
