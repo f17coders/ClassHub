342b72a84d069fc6861fb3dbefec5bafc924106e
하동준 <djh0211@konkuk.ac.kr>
2024-02-06 15:02:34 +0900

    [feat] 강의 정렬 기능 외 모든 api개발 완료 * S10P12A810-323 [fix] api path 수정 및 Token 적용
    * [fix] Security 도입으로 인한 Controller 수정 및 Member Service 수정
    * [feat] /api/*/v0 로 시작하는 api는 토큰 검증하지 않는 기능 구현
    
    * [feat] Member 도메인 Security 설정
    
    * [feat] registerMember(임시 메서드) 삭제
    
    * [fix] MemberSecurityDTO에 provider 추가
    - 추후 jobId도 추가해야함
    
    * [fix] 모든 controller에 Security 적용
    
    * S10P12A810-323 [fix] 태그 top5기능 단순화
    
    * [feat] oauth kakao 링크 연결
    
    * S10P12A810-323 [fix] api path 수정 및 Token 적용
    
    * S10P12A810-323 [fix] 태그 top5기능 단순화
    
    * [fix] Security 도입으로 인한 Controller 수정 및 Member Service 수정
    * [feat] /api/*/v0 로 시작하는 api는 토큰 검증하지 않는 기능 구현
    
    * [feat] Member 도메인 Security 설정
    
    * [feat] registerMember(임시 메서드) 삭제
    
    * [fix] MemberSecurityDTO에 provider 추가
    - 추후 jobId도 추가해야함
    
    * [fix] 모든 controller에 Security 적용
    
    * S10P12A810-323 [feat] 리뷰 등록, 수정, 삭제, 읽기 api 구현
    
    * S10P12A810-323 [fix] 태그 top5기능 단순화

---
+++ backend/src/main/java/com/f17coders/classhub/module/domain/review/dto/request/ReviewRegisterReq.java
@@ -0,0 +1,8 @@
+package com.f17coders.classhub.module.domain.review.dto.request;
+
+public record ReviewRegisterReq(
+	Float score,
+	String content
+) {
+
+}
--- backend/src/main/java/com/f17coders/classhub/module/domain/review/service/ReviewServiceImpl.java
+++ backend/src/main/java/com/f17coders/classhub/module/domain/review/service/ReviewServiceImpl.java
@@ -1,24 +1,35 @@
 package com.f17coders.classhub.module.domain.review.service;
 
+import com.f17coders.classhub.global.exception.BaseExceptionHandler;
+import com.f17coders.classhub.global.exception.code.ErrorCode;
+import com.f17coders.classhub.module.domain.lecture.Lecture;
+import com.f17coders.classhub.module.domain.lecture.repository.LectureRepository;
+import com.f17coders.classhub.module.domain.member.Member;
+import com.f17coders.classhub.module.domain.member.dto.response.MemberNickNameImageRes;
+import com.f17coders.classhub.module.domain.review.Review;
 import com.f17coders.classhub.module.domain.review.dto.response.ReviewListRes;
+import com.f17coders.classhub.module.domain.review.dto.request.ReviewRegisterReq;
 import com.f17coders.classhub.module.domain.review.dto.response.ReviewRes;
 import com.f17coders.classhub.module.domain.review.dto.response.SiteReviewListRes;
 import com.f17coders.classhub.module.domain.review.dto.response.SiteReviewRes;
 import com.f17coders.classhub.module.domain.review.repository.ReviewRepository;
 import com.f17coders.classhub.module.domain.review.repository.SiteReviewRepository;
-import com.f17coders.classhub.module.domain.study.service.StudyService;
+import jakarta.transaction.Transactional;
 import java.util.List;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 
+@Transactional
 @Service
 @RequiredArgsConstructor
 public class ReviewServiceImpl implements ReviewService {
 
 	private final ReviewRepository reviewRepository;
 	private final SiteReviewRepository siteReviewRepository;
+	private final LectureRepository lectureRepository;
 
 	@Override
 	public ReviewListRes getReviewList(int lectureId, Pageable pageable) {
@@ -47,6 +58,92 @@ public class ReviewServiceImpl implements ReviewService {
 		return SiteReviewListRes.builder()
 			.siteReviewResList(siteReviewList)
 			.totalPages(totalPages)
+			.build();
+	}
+
+	@Override
+	public int registerReview(int lectureId, ReviewRegisterReq reviewRegisterReq, Member member) {
+		Lecture lecture = lectureRepository.findById(lectureId)
+			.orElseThrow(
+				() -> new BaseExceptionHandler("lectureId=" + lectureId + " 강의를 DB에서 찾을수없습니다.",
+					ErrorCode.NOT_FOUND_ITEM_EXCEPTION));
+
+		Optional<Review> isReviewExist = reviewRepository.findByMember_MemberIdAndLecture_LectureId(
+			member.getMemberId(), lectureId);
+		if (isReviewExist.isPresent()) {
+			// 엔티티가 존재하면 이미 리뷰가 있는데 또 요청하는것이다.
+			throw new BaseExceptionHandler("해당 사용자는 해당 강의의 리뷰가 이미 존재합니다.",
+				ErrorCode.INTERNAL_SERVER_ERROR);
+		}
+		// 엔티티가 존재하지 않으면 새로운 Review 엔티티를 생성
+		Review review = Review.createReview(reviewRegisterReq.score(), reviewRegisterReq.content(),
+			member, lecture);
+		Review saveReview = reviewRepository.save(review);
+
+		return saveReview.getReviewId();
+	}
+
+	@Override
+	public int updateReview(int lectureId, ReviewRegisterReq reviewRegisterReq, Member member) {
+		Lecture lecture = lectureRepository.findById(lectureId)
+			.orElseThrow(
+				() -> new BaseExceptionHandler("lectureId=" + lectureId + " 강의를 DB에서 찾을수없습니다.",
+					ErrorCode.NOT_FOUND_ITEM_EXCEPTION));
+
+		Optional<Review> isReviewExist = reviewRepository.findByMember_MemberIdAndLecture_LectureId(
+			member.getMemberId(), lectureId);
+
+		if (!isReviewExist.isPresent()) {
+			throw new BaseExceptionHandler("해당 유저의 리뷰를 찾을수 없습니다.",
+				ErrorCode.NOT_FOUND_ITEM_EXCEPTION);
+			// reviewRepository.save(existingReview); // 이 부분은 필요하지 않을 수 있음
+		}
+
+		Review existingReview = isReviewExist.get();
+		existingReview.setContent(reviewRegisterReq.content());
+		existingReview.setScore(reviewRegisterReq.score());
+
+		return existingReview.getReviewId();
+	}
+
+	@Override
+	public void deleteReview(int lectureId, int memberId) {
+		Optional<Review> isReviewExist = reviewRepository.findByMember_MemberIdAndLecture_LectureId(
+			memberId, lectureId);
+		if (!isReviewExist.isPresent()) {
+			// 엔티티가 존재하지 않네. 사고다.
+			throw new BaseExceptionHandler("해당 리뷰가 존재하지 않습니다.",
+				ErrorCode.INTERNAL_SERVER_ERROR);
+		}
+
+		Review review = isReviewExist.get();
+
+		reviewRepository.delete(review);
+	}
+
+	@Override
+	public ReviewRes getMyLectureReview(int lectureId, Member member) {
+		Optional<Review> isReviewExist = reviewRepository.findByMember_MemberIdAndLecture_LectureId(
+			member.getMemberId(), lectureId);
+		if (!isReviewExist.isPresent()) {
+			// 아직 작성한 리뷰가 없다.
+			throw new BaseExceptionHandler("아직 리뷰가 존재하지 않습니다.", ErrorCode.NOT_FOUND_ITEM_EXCEPTION);
+		}
+
+		Review review = isReviewExist.get();
+		String replacedImageUrl = "https://simage-kr.uniqlo.com/goods/31/11/77/82/414920_COL_COL99_1000.jpg";
+
+		return ReviewRes.builder()
+			.reviewId(review.getReviewId())
+			.reviewId(review.getLecture().getLectureId())
+			.member(MemberNickNameImageRes.builder()
+				.nickname(member.getNickname())
+				.profileImage(
+					member.getProfileImage() == null ? replacedImageUrl : member.getProfileImage())
+				.build())
+			.createTime(review.getUpdateTime())
+			.score(review.getScore())
+			.content(review.getContent())
 			.build();
 	}
 }
