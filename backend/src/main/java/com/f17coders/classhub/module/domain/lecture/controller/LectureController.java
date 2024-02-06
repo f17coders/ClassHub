e1d1f98995c4bb08f029ecbf211e09990001bec8
djh0211 <djh0211@konkuk.ac.kr>
2024-02-06 14:44:23 +0900

    S10P12A810-323 [fix] api path 수정 및 Token 적용

--- backend/src/main/java/com/f17coders/classhub/module/domain/lecture/controller/LectureController.java
+++ backend/src/main/java/com/f17coders/classhub/module/domain/lecture/controller/LectureController.java
@@ -30,6 +30,7 @@ import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
