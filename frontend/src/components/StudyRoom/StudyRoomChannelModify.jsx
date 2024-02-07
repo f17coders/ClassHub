84cbcac84362b0ff5f19a00f0ff4f1a454b2ee1c
정유경 <kadie@naver.com>
2024-02-07 14:59:52 +0900

    S10P12A810-305 S10P12A810-307 S10P12A810-306 [feat] 스터디룸 채널 추가, 수정, 삭제 기능 구현 * S10P12A810-305 S10P12A810-306 S10P12A810-307 [feat] 스터디룸 채널 추가, 삭제, 수정 기능 구현

--- frontend/src/components/StudyRoom/StudyRoomChannelModify.jsx
+++ frontend/src/components/StudyRoom/StudyRoomChannelModify.jsx
@@ -1,70 +1,126 @@
 import React, { useState, useEffect } from 'react';
-import { useParams } from "react-router-dom";
-import { FormControl, OutlinedInput, Badge, Divider, List, ListItem,ListItemAvatar, Avatar,ListItemText, Button, Modal, Stack, TextField, Autocomplete, Box, Typography, Container, createFilterOptions, IconButton, Tooltip} from '@mui/material';
-import ChatIcon from '@mui/icons-material/Chat';
-import VerifiedIcon from '@mui/icons-material/Verified';
-import EditNoteIcon from '@mui/icons-material/EditNote';
-import HighlightOffIcon from '@mui/icons-material/HighlightOff';
+import { useNavigate, useParams } from "react-router-dom";
+import { TextField, Stack, FormControl, OutlinedInput, Button} from '@mui/material';
+
 import axios from 'axios';
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
 
-const style = {
-    position: 'absolute',
-    top: '45%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: "30%",
-    maxHeight: '80vh', // 최대 높이
-    overflowY: 'auto',  // 수직 스크롤 적용
-    bgcolor: 'background.paper',
-    border: '1px solid #000',
-    boxShadow: 24,
-    p: 4,
+export default function StudyRoomChannelModify({channelId, studyId, channelModifyClose, editingChannelId, setEditingChannelId}){
+  const MySwal = withReactContent(Swal);
+  const navigate = useNavigate();
+  const [content, setContent] = useState(''); //수정될 채널명
+  const accessToken = localStorage.getItem('token');
+  const [hasError, setHasError] = useState(false); // 입력값이 10자 이상인지 여부를 추적
+  const [isNull, setIsNull] = useState(false); // 입력값이 null인지 여부를 추적
+
+  // 입력값이 변경될 때마다 null 여부 업데이트
+  useEffect(() => {
+    setIsNull(!content); // content가 null이면 setIsNull을 true로 설정
+    setIsNull(content.length === 0);
+  }, [content]);
+
+  useEffect(() => {
+    setHasError(content.length > 10);
+  }, [content]);
+
+  // 취소 버튼 클릭 시 실행되는 함수
+  const cancelModification = () => {
+    // editingChannelId를 null로 변경하여 수정 모달이 닫힘을 알림
+    setEditingChannelId(null);
+    // 수정 모달 닫기
+    // channelModifyClose();
   };
-
-export default function StudyRoomChannelModify({channelId, studyRoomChannelModify,channelModifyClose}){
-    const [content, setContent] = useState(''); //수정될 채널명
-    const accessToken = localStorage.getItem('token');
-
-
-    // 채널 정보 수정
-    const modifyReply = (channelId) => {
-            axios.put(`https://i10a810.p.ssafy.io/api/studies/v1/channels`,{
-                "channelId": channelId,
-                "name": content,
-            }, {
-                headers: {
-                  Authorization: `Bearer ${accessToken}`,
-                },
-              })
-            .then((response)=> {
-                console.log(response.data.result)
-                channelModifyClose();
-                // window.location.reload(); //페이지 새로고침
+    
+    
+  // 채널 정보 수정
+  const modifyChannel = (channelId) => {
+          axios.put(`https://i10a810.p.ssafy.io/api/studies/v1/channels`,{
+              "channelId": channelId,
+              "name": content,
+          }, {
+              headers: {
+                Authorization: `Bearer ${accessToken}`,
+              },
             })
-            .catch((err) => console.log(err))
-        
+          .then((response)=> {
+              console.log(response.data.result)
+              channelModifyClose();
+              // 수정 완료 alert 띄우기
+              handleModifyDialogOpen();
+          })
+          .catch((err) => console.log(err))
+      
+  }
+  const enterKeyPress = (event, channelId) =>{
+    //엔터키 눌렀을 때 등록 함수 호출
+    if(event.key === 'Enter'){
+      event.preventDefault(); //기본 동작 방지
+      if(!isNull){
+        modifyChannel(channelId);
+      }
     }
+  }
+    
+  // 수정 확인 Dialog용
+  const handleModifyDialogOpen = () => {
+    MySwal.fire({
+      title: "수정 완료되었습니다!",
+      text: "채널 정보가 수정되었습니다.",
+      icon: "success",
+    }).then(() => {
+      window.location.reload();
+    }
+    )
+  }
 
     return(
         <>
-        {/* 채널 수정창 */}
-        <div style={{ width: '100%', marginTop: '10px'}}>
-                <form noValidate autoComplete="off">
-                  <FormControl size="small" sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
-                    <OutlinedInput 
-                    sx={{width: "100%", marginRight: '10px'}} 
+          {/* 채널 수정창 */}
+          <div style={{ width: '100%', marginTop: '10px', marginBottom: '10px'}}>
+            <Stack direction="row" spacing={1}>
+              {
+                hasError ? (
+                  <TextField
+                    error
+                    id="outlined-error-helper-text"
+                    size='small'
+                    sx = {{width: '100%'}}
+                    helperText="10자 이내로 입력하세요"
                     value={content}
                     onChange={(e) => setContent(e.target.value)}
-                    // onKeyDown={(e) => enterKeyPress(e, comment.commentId)} //엔터키 눌렀을 때 이벤트 핸들링
-                    />
-                    <Tooltip title="수정">
-                        <IconButton onClick={() => {modifyReply(channelId); }}>
-                            <VerifiedIcon/>
-                        </IconButton>
-                    </Tooltip>
-                  </FormControl>
-                </form>
-            </div>
+                    onKeyDown={(e) => enterKeyPress(e, channelId)}
+                  />
+                  ) : (
+                      <TextField
+                        required
+                        id="outlined-required"
+                        size='small'
+                        sx = {{width: '100%'}}
+                        value={content}
+                        onChange={(e) => setContent(e.target.value)}
+                        onKeyDown={(e) => enterKeyPress(e, channelId)}
+                      />
+                  )
+                }
+                
+                <Stack direction="row" spacing={1}>
+                  <Button variant="contained"
+                      size='small'
+                      onClick={() => {modifyChannel(channelId); }}
+                      disabled={hasError || isNull}>
+                        수정
+                    </Button>
+                    <Button variant="contained"
+                      size='small'
+                      color='error'
+                      onClick={cancelModification}
+                      >
+                        취소
+                    </Button>
+                </Stack>
+            </Stack>
+          </div>
         </>
     )
 }
