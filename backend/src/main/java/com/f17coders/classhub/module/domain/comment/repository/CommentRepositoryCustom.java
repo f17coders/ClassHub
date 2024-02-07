dd68537f6acf34263d0ce0f52ef33707420ca015
정승환 <seunghw2@naver.com>
2024-02-07 15:03:26 +0900

    [feat] Member Api 추가 구현 및 리팩터링 * [feat] 내가 스크랩한 글 보기 기능 구현
    * [feat] 내가 작성한 댓글의 글 보기 기능 구현
    - getCommentCommunityList에서 count 쿼리를 분리하여 구현
    
    * [refactor] memberId로 query하는 것이 아닌 member 객체로 query
    
    * [refactor] memberId로 query하는 것이 아닌 member 객체로 query
    
    * [feat] 내가 작성한 글 목록 가져오기 기능 추가
    - getCommunityList
    
    * [feat] 회원 탈퇴 기능 추가
    
    * [feat] 회원 탈퇴 기능 추가

--- backend/src/main/java/com/f17coders/classhub/module/domain/comment/repository/CommentRepositoryCustom.java
+++ backend/src/main/java/com/f17coders/classhub/module/domain/comment/repository/CommentRepositoryCustom.java
@@ -1,10 +1,4 @@
 package com.f17coders.classhub.module.domain.comment.repository;
 
-import com.f17coders.classhub.module.domain.comment.Comment;
-import com.f17coders.classhub.module.domain.community.Community;
-import com.f17coders.classhub.module.domain.tag.Tag;
-import java.util.List;
-import org.springframework.data.domain.Pageable;
-
 public interface CommentRepositoryCustom {
 }
--- backend/src/main/java/com/f17coders/classhub/module/domain/member/controller/MemberController.java
+++ backend/src/main/java/com/f17coders/classhub/module/domain/member/controller/MemberController.java
@@ -2,9 +2,9 @@ package com.f17coders.classhub.module.domain.member.controller;
 
 import com.f17coders.classhub.global.api.response.BaseResponse;
 import com.f17coders.classhub.global.exception.code.SuccessCode;
-import com.f17coders.classhub.module.domain.member.Member;
 import com.f17coders.classhub.module.domain.member.dto.request.MemberAddInfoReq;
 import com.f17coders.classhub.module.domain.member.dto.request.MemberUpdateInfoReq;
+import com.f17coders.classhub.module.domain.member.dto.response.MemberCommunityListRes;
 import com.f17coders.classhub.module.domain.member.dto.response.MemberGetInfoRes;
 import com.f17coders.classhub.module.domain.member.repository.MemberRepository;
 import com.f17coders.classhub.module.domain.member.service.MemberService;
@@ -14,16 +14,16 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.io.IOException;
 import java.util.List;
-import java.util.Optional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -41,7 +41,8 @@ public class MemberController {
     @GetMapping("/v1")
     public ResponseEntity<BaseResponse<MemberGetInfoRes>> getInformation(
         @AuthenticationPrincipal MemberSecurityDTO memberSecurityDTO) throws IOException {
-        MemberGetInfoRes memberGetInfoRes = memberService.getInformation(memberSecurityDTO.toMember());
+        MemberGetInfoRes memberGetInfoRes = memberService.getInformation(
+            memberSecurityDTO.toMember());
 
         return BaseResponse.success(SuccessCode.SELECT_SUCCESS, memberGetInfoRes);
     }
@@ -66,11 +67,53 @@ public class MemberController {
         return BaseResponse.success(SuccessCode.UPDATE_SUCCESS, memberSecurityDTO.getMemberId());
     }
 
+    @Operation(summary = "회원 탈퇴")
+    @DeleteMapping("/v1")
+    public ResponseEntity<BaseResponse<Integer>> withDraw(
+        @AuthenticationPrincipal MemberSecurityDTO memberSecurityDTO) throws IOException {
+        memberService.withDraw(memberSecurityDTO.toMember());
+
+        return BaseResponse.success(SuccessCode.DELETE_SUCCESS, memberSecurityDTO.getMemberId());
+    }
+
+    @Operation(summary = "내가 작성한 글 목록 조회")
+    @GetMapping("/v1/communities/my")
+    public ResponseEntity<BaseResponse<MemberCommunityListRes>> getCommunityList(Pageable pageable,
+        @AuthenticationPrincipal MemberSecurityDTO memberSecurityDTO) throws IOException {
+        MemberCommunityListRes communityList = memberService.getCommunityList(
+            memberSecurityDTO.toMember(), pageable);
+
+        return BaseResponse.success(SuccessCode.SELECT_SUCCESS, communityList);
+    }
+
+    @Operation(summary = "내가 작성한 댓글의 글 목록 조회")
+    @GetMapping("/v1/communities/comments")
+    public ResponseEntity<BaseResponse<MemberCommunityListRes>> getCommentCommunityList(
+        Pageable pageable,
+        @AuthenticationPrincipal MemberSecurityDTO memberSecurityDTO) throws IOException {
+        MemberCommunityListRes communityList = memberService.getCommentCommunityList(
+            memberSecurityDTO.toMember(), pageable);
+
+        return BaseResponse.success(SuccessCode.SELECT_SUCCESS, communityList);
+    }
+
+    @Operation(summary = "내가 스크랩한 글 목록 조회")
+    @GetMapping("/v1/communities/scraps")
+    public ResponseEntity<BaseResponse<MemberCommunityListRes>> getScrapCommunityList(
+        Pageable pageable,
+        @AuthenticationPrincipal MemberSecurityDTO memberSecurityDTO) throws IOException {
+        MemberCommunityListRes communityList = memberService.getScrapCommunityList(
+            memberSecurityDTO.toMember(), pageable);
+
+        return BaseResponse.success(SuccessCode.SELECT_SUCCESS, communityList);
+    }
+
     @Operation(summary = "내가 참여중인 스터디 목록 조회")
     @GetMapping("/v1/studies/participation")
     public ResponseEntity<BaseResponse<List<StudyBaseRes>>> getStudyList(
         @AuthenticationPrincipal MemberSecurityDTO memberSecurityDTO) throws IOException {
-        List<StudyBaseRes> studyBaseResList = memberService.getStudyList(memberSecurityDTO.toMember());
+        List<StudyBaseRes> studyBaseResList = memberService.getStudyList(
+            memberSecurityDTO.toMember());
 
         return BaseResponse.success(SuccessCode.SELECT_SUCCESS, studyBaseResList);
     }
