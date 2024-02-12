5ceff62643da8773b2aac87d217d01cdae121408
yeji <yeji11077@gmail.com>
2024-02-13 02:42:08 +0900

    [feat] 배너이미지 하나 추가

--- frontend/src/components/Home/Banner.jsx
+++ frontend/src/components/Home/Banner.jsx
@@ -1,32 +1,34 @@
 import banner1 from './../../assets/banner/banner1.png'
 import banner2 from './../../assets/banner/banner2.png'
 import banner3 from './../../assets/banner/banner3.png'
-import "react-responsive-carousel/lib/styles/carousel.min.css";
-import { Carousel } from 'react-responsive-carousel'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import { Autoplay, Navigation } from 'swiper/modules';
 
 // 홈페이지 메인 배너
 
 function Banner() {
 	return (
 		<div>
-			<Carousel
-				autoPlay
-				infiniteLoop
-				interval={7000}
-				showStatus={false}
-				showThumbs={false}
-				transitionTime={1000}
-			>
-				<div style={{ height: '100%' }}>
-					<img src={banner3} alt="" />
-				</div>
-				<div style={{ height: '100%' }}>
-					<img src={banner2} alt="" />
-				</div>
-				<div style={{ height: '100%' }}>
-					<img src={banner1} alt="" />
-				</div>
-			</Carousel>
+			<Swiper 
+			navigation={true} 
+			modules={[Navigation, Autoplay]} 
+			loop={true} 
+			autoplay={{
+				delay: 5000,
+				disableOnInteraction: false,
+			}}>
+				<SwiperSlide>
+					<img src={banner3}  />
+				</SwiperSlide>
+				<SwiperSlide>
+					<img src={banner2} style={{width:'100%'}} />
+				</SwiperSlide>
+				<SwiperSlide>
+					<img src={banner1} style={{width:'100%'}} />
+				</SwiperSlide>
+			</Swiper>
 		</div>
 	)
 }
