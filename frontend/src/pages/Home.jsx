0505575eade655ab0c3706817ba32151937220af
김예지 <yeji1107@naver.com>
2024-02-14 13:11:47 +0900

    [feat] 비로그인시 메인화면 * [feat] 비로그인시 메인화면

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -33,11 +33,11 @@ function Home() {
 		<div>
 			<Banner />
 			<Search />
-			
+
 			{
 				isLogin ? (
-					<div style={{margin:'auto', textAlign:'center', height:'65vh'}}>
-						<motion.div 
+					<div style={{ margin: 'auto', textAlign: 'center', height: '65vh' }}>
+						<motion.div
 							initial={{ opacity: 0, y: 40 }}
 							whileInView={{ opacity: 1, y: 0 }}
 							viewport={{ once: false }}
@@ -47,9 +47,9 @@ function Home() {
 								bounce: 0.2
 							}}
 							style={{ margin: '15vh 0vh 20vh 0vh' }}>
-								<p style={{fontSize:'3em', fontWeight:'800', paddingRight:'200px'}}>{user.nickname}님의 <span style={{color:'rgb(56, 65, 172)'}}>관심 기술</span>과 <span style={{color:'rgb(56, 65, 172)'}}>목표 직무</span>에 따라서</p>
-							</motion.div>
-							<motion.div 
+							<p style={{ fontSize: '3em', fontWeight: '800', paddingRight: '200px' }}>{user.nickname}님의 <span style={{ color: 'rgb(56, 65, 172)' }}>관심 기술</span>과 <span style={{ color: 'rgb(56, 65, 172)' }}>목표 직무</span>에 따라서</p>
+						</motion.div>
+						<motion.div
 							initial={{ opacity: 0, y: 40 }}
 							whileInView={{ opacity: 1, y: 0 }}
 							viewport={{ once: false }}
@@ -59,12 +59,54 @@ function Home() {
 								bounce: 0.2
 							}}
 							style={{ margin: '30vh 0vh 10vh 0vh' }}>
-								<p style={{fontSize:'3em', fontWeight:'800', paddingLeft:'300px'}}><span style={{color:'rgb(56, 65, 172)'}}>Classhub</span>가 강의를 추천해드려요</p>
-							</motion.div>
+							<p style={{ fontSize: '3em', fontWeight: '800', paddingLeft: '300px' }}><span style={{ color: 'rgb(56, 65, 172)' }}>Classhub</span>가 강의를 추천해드려요</p>
+						</motion.div>
 					</div>
-				) : null
+				) : (<div style={{ margin: 'auto', textAlign: 'center', height: '65vh' }}>
+					<motion.div
+						initial={{ opacity: 0, y: 40 }}
+						whileInView={{ opacity: 1, y: 0 }}
+						viewport={{ once: false }}
+						transition={{
+							type: "spring",
+							duration: 1.5,
+							bounce: 0.2
+						}}
+						style={{ margin: '15vh 0vh 20vh 0vh' }}>
+						<p style={{ fontSize: '3em', fontWeight: '800', paddingRight: '200px' }}><span style={{ color: 'rgb(56, 65, 172)' }}>회원 가입</span>을 통해 나에게 맞는</p>
+					</motion.div>
+					<motion.div
+						initial={{ opacity: 0, y: 40 }}
+						whileInView={{ opacity: 1, y: 0 }}
+						viewport={{ once: false }}
+						transition={{
+							type: "spring",
+							duration: 1.5,
+							bounce: 0.2
+						}}
+						style={{ margin: '30vh 0vh 10vh 0vh' }}>
+						<p style={{ fontSize: '3em', fontWeight: '800', paddingLeft: '300px' }}><span style={{ color: 'rgb(56, 65, 172)' }}>Classhub</span>의 추천을 받으세요</p>
+					</motion.div>
+				</div>)
 			}
-			<Divider/>
+			<Divider />
+			{
+				isLogin ? null : (
+					<motion.div
+						initial={{ opacity: 0, y: 40 }}
+						whileInView={{ opacity: 1, y: 0 }}
+						viewport={{ once: false }}
+						transition={{
+							type: "spring",
+							duration: 1.5,
+							bounce: 0.2
+						}}
+						style={{ margin: '0px', textAlign:'center' }}>
+						<p style={{ fontSize: '1.3em', color:'gray' }}>ClassHub의 인기 강의</p>
+					</motion.div>
+				)
+			}
+
 			<RecommendJob />
 			{
 				isLogin ? (<div>
