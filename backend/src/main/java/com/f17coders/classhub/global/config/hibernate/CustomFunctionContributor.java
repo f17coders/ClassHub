ba07d18696a6f7445872afa3e0917e6bb53fb098
djh0211 <djh0211@konkuk.ac.kr>
2024-02-15 13:48:26 +0900

    S10P12A810-323 [feat] 강의 - 검색기능 full-text-scan 인덱스 타게 사용자 정의 메서드 구현

---
+++ backend/src/main/java/com/f17coders/classhub/global/config/hibernate/CustomFunctionContributor.java
@@ -0,0 +1,16 @@
+package com.f17coders.classhub.global.config.hibernate;
+
+import org.hibernate.boot.model.FunctionContributions;
+import org.hibernate.boot.model.FunctionContributor;
+import org.hibernate.type.StandardBasicTypes;
+
+public class CustomFunctionContributor implements FunctionContributor {
+
+	@Override
+	public void contributeFunctions(FunctionContributions functionContributions) {
+		functionContributions.getFunctionRegistry()
+			.registerPattern("match_against", "match (?1) against (?2 in boolean mode)",
+				functionContributions.getTypeConfiguration().getBasicTypeRegistry()
+					.resolve(StandardBasicTypes.DOUBLE));
+	}
+}
